cf1b5882c3db078367fddbec0b7fdece075a6104
russellgoldenberg <russellgoldenberg@gmail.com>
2015-11-30 13:59:19 -0500

    missing data fixed

---
+++ data/create_gisjoin_1990.py
@@ -0,0 +1,30 @@
+import csv
+
+output = []
+
+def addRow(input, val):
+    newRow = []
+    for i in input:
+        newRow.append(i)
+
+    newRow.append(val)
+    output.append(newRow)
+
+with open('csv/raw_1990.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for i, row in enumerate(reader):
+        val = False
+        if i == 0:
+            val = 'gisjoin_mod'
+        else:	
+            gis = row[5]
+            if gis.endswith('0'):
+                val = row[6]
+            else: 
+                val = gis[:3] + '0' + gis[3:5] + '0' + gis[5:]
+        addRow(row, val)        
+
+writer = csv.writer(open('csv/raw_1990_modified.csv', 'w'))
+	
+for o in output:
+   writer.writerow(o)
